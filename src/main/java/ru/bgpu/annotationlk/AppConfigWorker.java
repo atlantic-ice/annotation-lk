1b8f7aa69d83cf01c811b283e94e123066bc922f
Nikita Lysansky <nikita.lysansky@yandex.com>
2025-02-20 16:46:38 +0900

    Поручение 3: Добавлена ​​поддержка нескольких типов поля, включая примитивы и массивы

--- src/main/java/ru/bgpu/annotationlk/AppConfigWorker.java
+++ src/main/java/ru/bgpu/annotationlk/AppConfigWorker.java
@@ -1,15 +1,15 @@
 package ru.bgpu.annotationlk;
 
 import org.reflections.Reflections;
-import org.reflections.scanners.FieldAnnotationsScanner;
+import org.reflections.scanners.Scanners;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Arrays;
 
 public class AppConfigWorker {
 
@@ -20,42 +20,39 @@ public class AppConfigWorker {
         Reflections reflections = new Reflections(prefix, Scanners.FieldsAnnotated);
 
         File prop = new File(filePropName);
-        if(prop.isFile()) {
+        if (prop.isFile()) {
             try {
                 Properties properties = new Properties();
                 properties.load(new FileInputStream(prop));
 
                 reflections.getFieldsAnnotatedWith(AppConfig.class).forEach(
                         field -> {
-
                             String value = properties.getProperty(
                                     field.getName(),
                                     field.getAnnotation(AppConfig.class).defValue()
                             );
-                            Object targetValue = null;
-
-                            if(field.getType().equals(String.class)) {
-                                targetValue = value;
-                            } else if(field.getType().equals(Integer.class)) {
-                                targetValue = Integer.valueOf(value);
-                            }
 
                             try {
                                 field.setAccessible(true);
+                                Object targetValue = convertValue(field.getType(), value);
                                 field.set(field.getDeclaringClass(), targetValue);
                                 field.setAccessible(false);
                             } catch (IllegalAccessException e) {
                                 logger.log(
                                         Level.WARNING,
-                                        "error set "+field.getDeclaringClass().getName()
-                                                +"."+field.getName()+" "+value
+                                        "error set " + field.getDeclaringClass().getName()
+                                                + "." + field.getName() + " " + value
+                                );
+                            } catch (IllegalArgumentException e) {
+                                logger.log(
+                                        Level.WARNING,
+                                        "error converting value for " + field.getDeclaringClass().getName()
+                                                + "." + field.getName() + " " + value
                                 );
                             }
-
-//                            System.out.println(field.getName());
                         }
                 );
-            } catch (Exception e) {
+            } catch (IOException e) {
                 logger.log(Level.WARNING, "error load properties", e);
             }
         } else {
@@ -63,4 +60,43 @@ public class AppConfigWorker {
         }
     }
 
+    private static Object convertValue(Class<?> fieldType, String value) {
+        if (fieldType == String.class) {
+            return value;
+        } else if (fieldType == int.class || fieldType == Integer.class) {
+            return Integer.parseInt(value);
+        } else if (fieldType == float.class || fieldType == Float.class) {
+            return Float.parseFloat(value);
+        } else if (fieldType == double.class || fieldType == Double.class) {
+            return Double.parseDouble(value);
+        } else if (fieldType == int[].class) {
+            return Arrays.stream(value.split(","))
+                         .mapToInt(Integer::parseInt)
+                         .toArray();
+        } else if (fieldType == float[].class) {
+            return Arrays.stream(value.split(","))
+                         .mapToFloat(Float::parseFloat)
+                         .toArray();
+        } else if (fieldType == double[].class) {
+            return Arrays.stream(value.split(","))
+                         .mapToDouble(Double::parseDouble)
+                         .toArray();
+        } else if (fieldType == Integer[].class) {
+            return Arrays.stream(value.split(","))
+                          .map(Integer::parseInt)
+                          .toArray(Integer[]::new);
+        } else if (fieldType == Float[].class) {
+            return Arrays.stream(value.split(","))
+                         .map(Float::parseFloat)
+                         .toArray(Float[]::new);
+        } else if (fieldType == Double[].class) {
+            return Arrays.stream(value.split(","))
+                         .map(Double::parseDouble)
+                         .toArray(Double[]::new);
+        } else if (fieldType == String[].class) {
+            return value.split(",");
+        } else {
+            throw new IllegalArgumentException("Unsupported field type: " + fieldType);
+        }
+    }
 }
